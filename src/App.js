9e5e8bca0b15118817566ebc076cc321b9242a9d
Alex Andrade <g.alex.andrade@gmail.com>
2019-03-14 16:58:22 -0600

    Creating Auth and Header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Todo from './Components/Todo';
+import Header from './Components/Header';
+import Auth from './Components/Auth';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <hr/>
         <Todo />
+        <Auth />
       </div>
     );
   }
