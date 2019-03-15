188919f009712831c24d971404bf1f011270f4e4
Alex Andrade <g.alex.andrade@gmail.com>
2019-03-15 08:48:58 -0600

    useContext descriptions

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import AuthContext from '../auth-context';
 
 const auth = props => {
+    /**
+     * `useContext` gets the context provided in a upper compoment
+     */
     const auth = useContext(AuthContext);
 
     return (
